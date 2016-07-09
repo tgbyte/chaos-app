ec79eef4f21f1096abe78ef26e13ced535e85546
Thilo-Alexander Ginkel <thilo@ginkel.com>
2016-07-09 22:41:25 +0200

    Syntax fix

--- app.rb
+++ app.rb
@@ -10,11 +10,11 @@ configure do
 end
 
 redis = Redis.new(host: REDIS_HOST)
-"Hello, Redis! ##{redis.incr('counter')}"
+"Hello, Redis! ##{redis.incr('starts')}"
 
 get '/' do
   redis = Redis.new(host: REDIS_HOST)
-  "Hello, Chaos! [req# #{redis.incr('counter')}, start# #{redis.get('starts'}]"
+  "Hello, Chaos! [req# #{redis.incr('counter')}, start# #{redis.get('starts')}]"
 end
 
 get '/fortune' do
